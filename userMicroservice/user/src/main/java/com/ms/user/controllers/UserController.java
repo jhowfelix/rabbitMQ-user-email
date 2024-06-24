df19c3d410d391b2731af76f56a9b506f0b4007b
jon-felix <jhow.wi56@gmail.com>
2024-06-24 20:32:04 -0300

    criação record user

--- userMicroservice/user/src/main/java/com/ms/user/controllers/UserController.java
+++ userMicroservice/user/src/main/java/com/ms/user/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ms.user.dtos.UserRecordDto;
 import com.ms.user.models.UserModel;
 
 import jakarta.validation.Valid;
