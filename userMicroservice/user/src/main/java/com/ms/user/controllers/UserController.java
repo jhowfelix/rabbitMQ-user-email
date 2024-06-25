79b56201e7cc121408a1a59deac77a4007a951f9
jon-felix <jhow.wi56@gmail.com>
2024-06-25 17:06:13 -0300

    criação endpoint para salver usuários, alteração db

--- userMicroservice/user/src/main/java/com/ms/user/controllers/UserController.java
+++ userMicroservice/user/src/main/java/com/ms/user/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.ms.user.controllers;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,16 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ms.user.dtos.UserRecordDto;
 import com.ms.user.models.UserModel;
+import com.ms.user.services.UserService;
 
 import jakarta.validation.Valid;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    UserService service;
+
     @PostMapping("/users")
     public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto UserRecordDto){
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(UserRecordDto, userModel);
+        UserModel user = service.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
 
